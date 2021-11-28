78c179780f86411c246556b4fc7794e31c504195
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-11-28 21:18:45 +0100

    minnor

--- webshop-frontend/src/app/material.module.ts
+++ webshop-frontend/src/app/material.module.ts
@@ -6,6 +6,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     exports: [
@@ -14,7 +17,10 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
         MatToolbarModule,
         MatMenuModule,
         MatIconModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule
     ]
 })
 export class MaterialModule { }
