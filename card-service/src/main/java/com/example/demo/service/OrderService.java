860ac8e13976d1b930ff176409e567b32016e51d
unknown <nikolicpetar91@yahoo.com>
2021-12-04 20:49:07 +0100

    ciphering possibly complete

--- card-service/src/main/java/com/example/demo/service/OrderService.java
+++ card-service/src/main/java/com/example/demo/service/OrderService.java
@@ -73,10 +73,10 @@ public class OrderService {
 
 		PaymentRequestDTO dto = paymentRequestMapper.toDTO(merchant, order);
 
-		log.info("pay - create PaymentRequest in bank @" + merchant.getBankUrl());
+		log.info("pay - create PaymentRequest in bank @" + cipher.decrypt(merchant.getBankUrl()));
 		ResponseEntity<PaymentRequestResponseDTO> responseEntity = restTemplate.exchange(
-				merchant.getBankUrl() + "/payment-requests", HttpMethod.POST, new HttpEntity<PaymentRequestDTO>(dto),
-				PaymentRequestResponseDTO.class);
+				cipher.decrypt(merchant.getBankUrl()) + "/payment-requests", HttpMethod.POST,
+				new HttpEntity<PaymentRequestDTO>(dto), PaymentRequestResponseDTO.class);
 
 		this.save(order);
 
