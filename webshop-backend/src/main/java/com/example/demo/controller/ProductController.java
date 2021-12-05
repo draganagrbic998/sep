e788ff7b370bc5f37f04784cbba7a3d9357e4e56
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-12-05 02:52:56 +0100

    webshop preauthorize

--- webshop-backend/src/main/java/com/example/demo/controller/ProductController.java
+++ webshop-backend/src/main/java/com/example/demo/controller/ProductController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -26,6 +27,7 @@ import lombok.extern.log4j.Log4j2;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/products")
+@PreAuthorize("hasAuthority('merchant')")
 @Log4j2
 public class ProductController {
 
