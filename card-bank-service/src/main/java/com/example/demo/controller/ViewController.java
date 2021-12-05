96d7a23423123b469854f76ede2d333ba7f19d86
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-12-05 21:31:48 +0100

    view controller card bank update

--- card-bank-service/src/main/java/com/example/demo/controller/ViewController.java
+++ card-bank-service/src/main/java/com/example/demo/controller/ViewController.java
@@ -1,23 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/view")
 public class ViewController {
 
-	@RequestMapping("/form/{paymentRequestId}")
-	public String form(@PathVariable Long paymentRequestId, Model model) {
-		model.addAttribute("paymentRequestId", paymentRequestId);
-		return "form";
+	@RequestMapping("/registerForm")
+	public String registerForm() {
+		return "registerForm";
 	}
 
-	@RequestMapping("/registerForm")
-	public String form(Model model) {
-		return "registerForm";
+	@RequestMapping("/paymentForm/{paymentRequestId}")
+	public String form() {
+		return "paymentForm";
 	}
 
 }
