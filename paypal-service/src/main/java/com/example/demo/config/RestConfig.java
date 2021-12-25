93c9351a5c0b65a4d1dc342191925712ff2169f5
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-12-25 09:56:45 +0100

    paypal minnor

--- paypal-service/src/main/java/com/example/demo/config/RestConfig.java
+++ paypal-service/src/main/java/com/example/demo/config/RestConfig.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.KeyStore;
 
-import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
@@ -35,17 +34,13 @@ public class RestConfig {
 			keyStore.load(in, "password".toCharArray());
 			in.close();
 
-			SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
-					new SSLContextBuilder().loadTrustMaterial(keyStore, new TrustSelfSignedStrategy())
-							.loadKeyMaterial(keyStore, "password".toCharArray()).build(),
-					NoopHostnameVerifier.INSTANCE);
-
-			HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).setMaxConnTotal(5)
-					.setMaxConnPerRoute(5).build();
-
 			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
-					httpClient);
-
+					HttpClients.custom()
+							.setSSLSocketFactory(new SSLConnectionSocketFactory(
+									new SSLContextBuilder().loadTrustMaterial(keyStore, new TrustSelfSignedStrategy())
+											.loadKeyMaterial(keyStore, "password".toCharArray()).build(),
+									NoopHostnameVerifier.INSTANCE))
+							.setMaxConnTotal(5).setMaxConnPerRoute(5).build());
 			requestFactory.setReadTimeout(10000);
 			requestFactory.setConnectTimeout(10000);
 			restTemplate.setRequestFactory(requestFactory);
--- paypal-service/src/main/java/com/example/demo/utils/DatabaseCipher.java
+++ paypal-service/src/main/java/com/example/demo/utils/DatabaseCipher.java
@@ -26,11 +26,12 @@ public class DatabaseCipher {
 	private IvParameterSpec ips;
 
 	public String encrypt(String plainText) {
-		if (plainText == null || plainText.isBlank())
+		if (plainText.isBlank()) {
 			return plainText;
+		}
 		try {
-			this.cipher.init(Cipher.ENCRYPT_MODE, this.key, this.ips);
-			return Base64.getEncoder().encodeToString(this.cipher.doFinal(plainText.getBytes()));
+			cipher.init(Cipher.ENCRYPT_MODE, key, ips);
+			return Base64.getEncoder().encodeToString(cipher.doFinal(plainText.getBytes()));
 		} catch (InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException
 				| BadPaddingException e) {
 			throw new RuntimeException(e);
@@ -38,83 +39,81 @@ public class DatabaseCipher {
 	}
 
 	public String decrypt(String cipherText) {
-		if (cipherText == null || cipherText.isBlank())
-			return cipherText;
 		try {
-			this.cipher.init(Cipher.DECRYPT_MODE, this.key, this.ips);
-			return new String(this.cipher.doFinal(Base64.getDecoder().decode(cipherText)));
+			cipher.init(Cipher.DECRYPT_MODE, key, ips);
+			return new String(cipher.doFinal(Base64.getDecoder().decode(cipherText)));
 		} catch (InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException
 				| BadPaddingException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public Merchant encrypt(Merchant m) {
-		m.setClientId(this.encrypt(m.getClientId()));
-		m.setClientSecret(this.encrypt(m.getClientSecret()));
-		m.setMerchantApiKey(this.encrypt(m.getMerchantApiKey()));
-		return m;
+	public Merchant encrypt(Merchant merchant) {
+		merchant.setClientId(encrypt(merchant.getClientId()));
+		merchant.setClientSecret(encrypt(merchant.getClientSecret()));
+		merchant.setMerchantApiKey(encrypt(merchant.getMerchantApiKey()));
+		return merchant;
 	}
 
-	public Merchant decrypt(Merchant m) {
-		m.setClientId(this.decrypt(m.getClientId()));
-		m.setClientSecret(this.decrypt(m.getClientSecret()));
-		m.setMerchantApiKey(this.decrypt(m.getMerchantApiKey()));
-		return m;
+	public Merchant decrypt(Merchant merchant) {
+		merchant.setClientId(decrypt(merchant.getClientId()));
+		merchant.setClientSecret(decrypt(merchant.getClientSecret()));
+		merchant.setMerchantApiKey(decrypt(merchant.getMerchantApiKey()));
+		return merchant;
 	}
 
-	public Order encrypt(Order o) {
-		o.setPayPalOrderId(this.encrypt(o.getPayPalOrderId()));
-		o.setMerchantApiKey(this.encrypt(o.getMerchantApiKey()));
-		return o;
+	public Order encrypt(Order order) {
+		order.setPayPalOrderId(encrypt(order.getPayPalOrderId()));
+		order.setMerchantApiKey(encrypt(order.getMerchantApiKey()));
+		return order;
 	}
 
-	public Order decrypt(Order o) {
-		o.setPayPalOrderId(this.decrypt(o.getPayPalOrderId()));
-		o.setMerchantApiKey(this.decrypt(o.getMerchantApiKey()));
-		return o;
+	public Order decrypt(Order order) {
+		order.setPayPalOrderId(decrypt(order.getPayPalOrderId()));
+		order.setMerchantApiKey(decrypt(order.getMerchantApiKey()));
+		return order;
 	}
 
-	public Product encrypt(Product p) {
-		p.setProductId(this.encrypt(p.getProductId()));
-		p.setName(this.encrypt(p.getName()));
-		return p;
+	public Product encrypt(Product product) {
+		product.setProductId(encrypt(product.getProductId()));
+		product.setName(encrypt(product.getName()));
+		return product;
 	}
 
-	public Product decrypt(Product p) {
-		p.setProductId(this.decrypt(p.getProductId()));
-		p.setName(this.decrypt(p.getName()));
-		return p;
+	public Product decrypt(Product product) {
+		product.setProductId(decrypt(product.getProductId()));
+		product.setName(decrypt(product.getName()));
+		return product;
 	}
 
-	public BillingPlan encrypt(BillingPlan b) {
-		b.setPlanId(this.encrypt(b.getPlanId()));
-		b.setProductId(this.encrypt(b.getProductId()));
-		b.setName(this.encrypt(b.getName()));
-		return b;
+	public BillingPlan encrypt(BillingPlan plan) {
+		plan.setPlanId(encrypt(plan.getPlanId()));
+		plan.setProductId(encrypt(plan.getProductId()));
+		plan.setName(encrypt(plan.getName()));
+		return plan;
 	}
 
-	public BillingPlan decrypt(BillingPlan b) {
-		b.setPlanId(this.decrypt(b.getPlanId()));
-		b.setProductId(this.decrypt(b.getProductId()));
-		b.setName(this.decrypt(b.getName()));
-		return b;
+	public BillingPlan decrypt(BillingPlan plan) {
+		plan.setPlanId(decrypt(plan.getPlanId()));
+		plan.setProductId(decrypt(plan.getProductId()));
+		plan.setName(decrypt(plan.getName()));
+		return plan;
 	}
 
-	public Subscription encrypt(Subscription s) {
-		s.setApproveUrl(this.encrypt(s.getApproveUrl()));
-		s.setSubscriber(this.encrypt(s.getSubscriber()));
-		s.setSubscriptionId(this.encrypt(s.getSubscriptionId()));
-		s.setPlanId(this.encrypt(s.getPlanId()));
-		return s;
+	public Subscription encrypt(Subscription subscription) {
+		subscription.setApproveUrl(encrypt(subscription.getApproveUrl()));
+		subscription.setSubscriber(encrypt(subscription.getSubscriber()));
+		subscription.setSubscriptionId(encrypt(subscription.getSubscriptionId()));
+		subscription.setPlanId(encrypt(subscription.getPlanId()));
+		return subscription;
 	}
 
-	public Subscription decrypt(Subscription s) {
-		s.setApproveUrl(this.decrypt(s.getApproveUrl()));
-		s.setSubscriber(this.decrypt(s.getSubscriber()));
-		s.setSubscriptionId(this.decrypt(s.getSubscriptionId()));
-		s.setPlanId(this.decrypt(s.getPlanId()));
-		return s;
+	public Subscription decrypt(Subscription subscription) {
+		subscription.setApproveUrl(decrypt(subscription.getApproveUrl()));
+		subscription.setSubscriber(decrypt(subscription.getSubscriber()));
+		subscription.setSubscriptionId(decrypt(subscription.getSubscriptionId()));
+		subscription.setPlanId(decrypt(subscription.getPlanId()));
+		return subscription;
 	}
 
 }
