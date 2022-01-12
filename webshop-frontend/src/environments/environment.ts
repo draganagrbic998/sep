da30e518d315eff53d3de834133fc5ec52ff9351
unknown <nikolicpetar91@yahoo.com>
2022-01-12 23:59:30 +0100

    minor

--- webshop-frontend/src/environments/environment.ts
+++ webshop-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:8080',
-  selectPaymentMethodUrl: 'https://10.168.44.109:8081/view/selectPaymentMethod'
+  selectPaymentMethodUrl: 'https://localhost:8081/view/selectPaymentMethod'
 };
